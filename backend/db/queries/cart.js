e2dd977ec0f0efe81bdb8b63d43ddf5b281dad54
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-16 11:35:15 -0700

    Add queries

---
+++ backend/db/queries/cart.js
@@ -0,0 +1,11 @@
+const getOrdersByID = (id) => {
+  return client
+    .query(
+      "SELECT order_items.* FROM orders JOIN order_items ON order_id = orders.id WHERE order_id = $1",
+      [id]
+    )
+    .then((order) => {
+      console.log("getOrders function", order.rows);
+      return order.rows;
+    });
+};
