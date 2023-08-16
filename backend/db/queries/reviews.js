a0219d9f4d60293248d129740d18ec4bc8f174ca
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-08-16 14:22:23 -0600

    create reviews query

--- backend/db/queries/reviews.js
+++ backend/db/queries/reviews.js
@@ -1,22 +1,31 @@
-// const client = require("../connection");
+const client = require("../connection");
 
-// //Get all reviews
-// const getOrders = () => {
-//   return client
-//     .query("SELECT * FROM reviews")
-//     .then((reviews) => {
-//       return reviews.rows;
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+//Get all reviews
+const getReviews = () => {
+  return client
+    .query("SELECT * FROM reviews")
+    .then((reviews) => {
+      return reviews.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 
-// //Write a new review
-// const writeReview = (review) => {
-//   return client
-//   .query(
-//     `INSERT INTO reviews (customer_id, message, rating, created_at) VALUES ($1, $2, $3, $4)`
-//   )
-// }
+//Write a new review
+const writeReview = (review) => {
+  return client
+    .query(
+      `INSERT INTO reviews (customer_id, message, rating, created_at) VALUES ($1, $2, $3, $4) RETURNING *`, [review.customer_id, review.message, review.rating, review.created_at]
+    )
+    .then((review) => {
+      return review.rows[0];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+
+module.exports = { getReviews, writeReview };
